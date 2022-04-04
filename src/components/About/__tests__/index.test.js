4e94645b4e4506e0fde7d349006e42856f0a1fca
NXF1LT3R <eplecha87@gmail.com>
2022-04-04 17:08:19 -0400

    create the photo gallery

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,4 +1,3 @@
-// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,7 +5,7 @@ import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
+describe('About component renders', () => {
   it('renders', () => {
     render(<About />);
   });
